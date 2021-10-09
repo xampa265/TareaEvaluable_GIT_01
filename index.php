da4d1bc1f1df39ec6c1017ce831935a1a5e7fa8f
Amparo Moreno <ammoba@gmail.com>
2021-10-09 23:37:57 +0200

    agragando codigo a index desde test_index

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 echo "hola con git";
 echo " modifico esto para subirlo al repositorio";
 echo "he modificado esto para trabajar en mi nueva rama  test index y que haya cambios"
-
+// quiero hacer un cambio en index.php desde test index
  ?>
