b8830103cb51f69ade6771d6027bb648d885c4a3
Amparo Moreno <ammoba@gmail.com>
2021-10-11 10:33:11 +0200

    aceptados los cambios de test index 3

--- index.php
+++ index.php
@@ -2,21 +2,5 @@
 
 echo "hola con git";
 echo " modifico esto para subirlo al repositorio";
-<<<<<<< HEAD
 
-echo "modifico en main index.php";
-
-echo "he modificado esto para trabajar en mi nueva rama  test index y que haya cambios";
-
-
-
-
-
-
-// haciendo cambios en index.php desde main
-
-=======
-echo "he modificado esto para trabajar en mi nueva rama  test index y que haya cambios"
-// quiero hacer un cambio en index.php desde test index
->>>>>>> test_index
  ?>
